1297fa1004924892e691ae3b4efd045a4b48489b
timeOfreedom <673462121@qq.com>
2019-03-08 20:16:56 +0800

    最终效果达成，现在需要three的版本

--- v1n2/cube.js
+++ v1n2/cube.js
@@ -1,8 +1,50 @@
+class Node {
+    link = [];
+    value = null;
+    visited = false;
+    constructor(value = null) {
+        this.value = value;
+    }
+    linkNode(node) {
+        this.link.push(node);
+        node.link.push(this);
+    }
+    unlinkNode(node) {
+        let index = this.link.indexOf(node);
+        this.link.splice(index, 1);
+        index = node.link.indexOf(this);
+        node.link.splice(index, 1);
+    }
+}
+
+function searchAllLinkOFNodes(tnodes) {
+    var a = [];
+    tnodes.forEach((e) => {
+        a = a.concat(e.link.map((e1) => [e1,e]));
+    });
+    a = [...new Set(a)];
+    return a;
+}
+class Line {
+    constructor(s, e) {
+        this.s = s;
+        this.e = e;
+    }
+}
 class Cube {
     points = [];
     lines = [];
-    constructor() {
+    indexNodeMap = [];
+    level = 0;
+    constructor(level, x, y, size = 100, lineColor = '#0000ff') {
         this.initPoints();
+        
+        this.level = level;
+        this.initNodeMap();
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.lineColor = lineColor;
     }
     initPoints() {
         let pt = {
@@ -16,24 +58,134 @@ class Cube {
                     pt.x = .5 * ((i % 2) - .5) * 2;
                     pt.y = .5 * ((j % 2) - .5) * 2;
                     pt.z = .5 * ((k % 2) - .5) * 2;
-                    this.points.push({x:pt.x, y:pt.y, z:pt.z});
+                    this.points.push({
+                        x: pt.x,
+                        y: pt.y,
+                        z: pt.z
+                    });
                 }
             }
         }
     }
-    lineRender() {
-        this.lines.forEach((e, i, a) => {
-            stroke(lineColor);
-            strokeWeight(lineWidth);
-            line(e.s.x, e.s.y, e.s.z, e.e.x, e.e.y, e.e.z);
-        })
+    initNodeMap() {
+        this.indexNodeMap = [];
+        for (let i = 0; i < this.points.length; i++) {
+            this.indexNodeMap.push(new Node(i));
+        }
+        for (let k = 0; k < this.level; k++) {
+            let s = randomInt(this.points.length);
+            let e = randomInt(this.points.length);
+            let i = 0;
+            while (e == s || this.indexNodeMap[s].link.includes(this.indexNodeMap[e]) && i < 15) {
+                e = randomInt(this.points.length);
+                i++;
+            }
+            this.indexNodeMap[s].linkNode(this.indexNodeMap[e]);
+        }
+
+        function randomInt(n) {
+            return Math.floor(Math.random() * n);
+        }
     }
-    
-    ptRender(){
+    rotateZa(angle) {
+        var rotateMatrix = [
+            [Math.cos(angle), -Math.sin(angle), 0],
+            [Math.sin(angle), Math.cos(angle), 0],
+            [0, 0, 1]
+        ];
+        var newPoints = [];
+        for (let i = 0; i < this.points.length; i++) {
+            let tPtBuffer = [
+                [this.points[i].x],
+                [this.points[i].y],
+                [this.points[i].z]
+            ];
+            tPtBuffer = math.multiply(rotateMatrix, tPtBuffer);
+            newPoints.push({
+                x: tPtBuffer[0][0],
+                y: tPtBuffer[1][0],
+                z: tPtBuffer[2][0]
+            });
+        }
+        this.points = newPoints;
+    }
+    rotateYa(angle) {
+        var rotateMatrix = [
+            [Math.cos(angle), 0, Math.sin(angle)],
+            [0, 1, 0],
+            [-Math.sin(angle), 0, Math.cos(angle)]
+        ];
+        var newPoints = [];
+        for (let i = 0; i < this.points.length; i++) {
+            let tPtBuffer = [
+                [this.points[i].x],
+                [this.points[i].y],
+                [this.points[i].z]
+            ];
+            tPtBuffer = math.multiply(rotateMatrix, tPtBuffer);
+            newPoints.push({
+                x: tPtBuffer[0][0],
+                y: tPtBuffer[1][0],
+                z: tPtBuffer[2][0]
+            });
+        }
+        this.points = newPoints;
+    }
+    rotateXa(angle) {
+        var rotateMatrix = [
+            [1, 0, 0],
+            [0, Math.cos(angle), -Math.sin(angle)],
+            [0, Math.sin(angle), Math.cos(angle)]
+        ];
+        var newPoints = [];
+        for (let i = 0; i < this.points.length; i++) {
+            let tPtBuffer = [
+                [this.points[i].x],
+                [this.points[i].y],
+                [this.points[i].z]
+            ];
+            tPtBuffer = math.multiply(rotateMatrix, tPtBuffer);
+            newPoints.push({
+                x: tPtBuffer[0][0],
+                y: tPtBuffer[1][0],
+                z: tPtBuffer[2][0]
+            });
+        }
+        this.points = newPoints;
+    }
+
+
+
+    lineRender() {
+        this.lines = [];
+        var allLink = searchAllLinkOFNodes(this.indexNodeMap);
+        this.lines = allLink.map((e) => new Line(this.points[e[0].value], this.points[e[1].value]));
+        push();
+        translate(this.x, this.y);
+        this.lines.forEach((e, i, a) => {
+            stroke(this.lineColor);
+            strokeWeight(lineWidth);
+            strokeCap(ROUND);
+            line(e.s.x * this.size, e.s.y * this.size, e.e.x * this.size, e.e.y * this.size);
+        });
+        pop();
+    }
+
+    ptRender() {
         this.points.forEach((e) => {
-            e.z = 0;
             stroke('#ffffff');
             point(e.x * 100, e.y * 100);
         });
     }
+}
+
+function structuralClone(obj) {
+    return new Promise(resolve => {
+        const {
+            port1,
+            port2
+        } = new MessageChannel();
+        port2.onmessage = ev => resolve(ev.data);
+        port1.postMessage(obj);
+    });
 }
--- v1n2/main.js
+++ v1n2/main.js
@@ -1,26 +1,113 @@
 var bgColor = '#000000';
-var lineColor = '#000000';
-var lineWidth = 3;
+var lineColor = `rgb(255, 0, 0)`;
+var lineWidth = 10;
+var viewWindow = {
+    width: window.innerWidth * .618,
+    height: window.innerWidth * .618 * .618
+}
+var xNum = 30,
+    yNum = 20,
+    xScl = Math.floor(viewWindow.width / xNum),
+    yScl = Math.floor(viewWindow.height / yNum);
+var cellScl = .4;
+
 function init() {
     createCanvas(window.innerWidth, window.innerHeight);
     gridRender();
 }
-function gridRender(){
+
+function gridRender() {
 
 }
 
+var cubes = [];
+var number = 3;
+var direction = ['r', 'd', 'l', 'u'];
+direction.turnRight = function (index) {
+    return index < direction.length - 1 ? ++index : 0;
+}
+direction.turnLeft = function (index) {
+    return index > 0 ? --index : direction.length - 1;
+}
+var totalTime = 15;
+perfectCube(0,
+    0,
+    window.innerWidth * .5,
+    window.innerWidth * .5 * .618,
+    0,
+    totalTime);
 
-console.log(math.multiply(c, d));   
+function perfectCube(sx, sy, w, l, d, times) {
+    if (times > 10){
+        lineColor = '#000055';
+    } else {
+        lineColor = '#cc0000';
+    }
+    if (times == 0) {
+
+    } else {
+        let cx;
+        let cy;
+        let nx, ny;
+        let len = Math.min(w, l);
+
+        switch (d) {
+            case 0:
+                cx = sx + len / 2;
+                cy = sy + len / 2;
+                nx = sx + w;
+                ny = sy;
+                break;
+            case 1:
+                cx = sx - len / 2;
+                cy = sy + len / 2;
+                nx = sx;
+                ny = sy + w;
+                break;
+            case 2:
+                cx = sx - len / 2;
+                cy = sy - len / 2;
+                nx = sx - w;
+                ny = sy;
+                break;
+            case 3:
+                cx = sx + len / 2;
+                cy = sy - len / 2;
+                nx = sx;
+                ny = sy - w;
+
+        }
+
+        cubes.push(new Cube(1, cx, cy, len * .9, lineColor));
+        perfectCube(nx, ny, len, Math.max(w, l) - len, direction.turnRight(d), --times);
+    }
+}
+
+document.addEventListener('keypress', function (e) {
+    if (e.key == 'w') {
+        cubes.forEach((e) => e.level++);
+    } else {
+        if (e.key == 's') {
+            cubes.forEach((e) => e.level--);
+        }
+    }
+    cubes.forEach((e) => e.initNodeMap());
+})
+
 function setup() {
     init();
 }
 
 function draw() {
     background(bgColor)
-    translate(width/2, height/2);
-    testCube.ptRender();
+    let scl = .0004;
+    translate(window.innerWidth / 2 - window.innerWidth * .5 /2, window.innerHeight / 2 - window.innerWidth * .5 * .618 / 2);
+    cubes.forEach((e) => e.rotateYa(abs(mouseX - cubes[cubes.length - 1].x - (window.innerWidth / 2 - window.innerWidth * .5 /2)) * scl));
+    cubes.forEach((e) => e.rotateXa(abs(mouseY - cubes[cubes.length - 1].y - (window.innerHeight / 2 - window.innerWidth * .5 * .618 / 2)) * scl));
+    cubes.forEach((e) => e.lineRender());
+    
 }
 
-function windowResized(){
+function windowResized() {
     init();
 }
